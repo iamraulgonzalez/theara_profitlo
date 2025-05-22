b3c372cac2a7f827c84d2dbe6e0fe50424a07676
Theara Ren <135191629+ThearaSR@users.noreply.github.com>
2025-05-23 00:49:45 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ export default function App() {
       <main className="flex-1 flex justify-center px-2 sm:px-4 pt-16 sm:pt-20">
         <Profilo
           name="រ៉េន ធារ៉ា"
-          projectName="Profitlo"
+          projectName="Theara Ren"
           profileImageSrc={profileImage}
         />
       </main>
       <footer className="text-center text-gray-500 py-3 sm:py-4 px-4 text-xs sm:text-sm">
         <div className="max-w-7xl mx-auto">
-          &copy; {new Date().getFullYear()} Profitlo. All rights reserved.
+          &copy; {new Date().getFullYear()} Theara Ren. All rights reserved.
         </div>
       </footer>
     </div>
